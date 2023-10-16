8c3f2aa8349cb5cedbe5bdded67353e8bd8ec3c1
punit <punit.pradhan@innoraft.com>
2023-10-16 19:40:20 +0530

    second half completed

--- web/themes/custom/module_5_test/gulpfile.js
+++ web/themes/custom/module_5_test/gulpfile.js
@@ -1,18 +1,76 @@
-const { series } = require('gulp');
-
-// The `clean` function is not exported so it can be considered a private task.
-// It can still be used within the `series()` composition.
-function clean(cb) {
-  // body omitted
-  cb();
+let gulp = require('gulp'),
+  sass = require('gulp-sass')(require('sass')),
+  sourcemaps = require('gulp-sourcemaps'),
+  $ = require('gulp-load-plugins')(),
+  cleanCss = require('gulp-clean-css'),
+  rename = require('gulp-rename'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  postcssInlineSvg = require('postcss-inline-svg'),
+  browserSync = require('browser-sync').create()
+pxtorem = require('postcss-pxtorem'),
+  postcssProcessors = [
+    postcssInlineSvg({
+      removeFill: true,
+      // paths: ['./node_modules/bootstrap-icons/icons']
+    }),
+    pxtorem({
+      propList: ['font', 'font-size', 'line-height', 'letter-spacing', '*margin*', '*padding*'],
+      mediaQuery: true
+    })
+  ];
+const paths = {
+  scss: {
+    src: './scss/**/*.scss',
+    dest: './css',
+    watch: './scss/**/*.scss',
+  },
+  js: {
+    // bootstrap: './node_modules/bootstrap/dist/js/bootstrap.min.js',
+    // popper: './node_modules/@popperjs/core/dist/umd/popper.min.js',
+    // barrio: '../../contrib/bootstrap_barrio/js/barrio.js',
+    dest: './js'
+  }
 }
-
-// The `build` function is exported so it is public and can be run with the `gulp` command.
-// It can also be used within the `series()` composition.
-function build(cb) {
-  // body omitted
-  cb();
+// Compile sass into CSS & auto-inject into browsers
+function styles() {
+  return gulp.src([paths.scss.src])
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      includePaths: [
+        // './node_modules/bootstrap/scss',
+        // '../../contrib/bootstrap_barrio/scss',
+        './scss'
+      ]
+    }).on('error', sass.logError))
+    .pipe($.postcss(postcssProcessors))
+    .pipe(postcss([autoprefixer({
+      overrideBrowserslist: [
+        'Chrome >= 35',
+        'Firefox >= 38',
+        'Edge >= 12',
+        'Explorer >= 10',
+        'iOS >= 8',
+        'Safari >= 8',
+        'Android 2.3',
+        'Android >= 4',
+        'Opera >= 12']
+    })]))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.scss.dest))
+    .pipe(cleanCss())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(paths.scss.dest))
+    .pipe(browserSync.stream())
 }
-
-exports.build = build;
-exports.default = series(clean, build);
+// Static Server + watching scss/html files
+function serve() {
+  /*browserSync.init({
+    proxy: 'https://www.drupal.org',
+  })*/
+  gulp.watch([paths.scss.watch], styles).on('change', browserSync.reload)
+}
+const build = gulp.series(styles, gulp.parallel(serve))
+exports.styles = styles
+exports.serve = serve
+exports.default = build
--- web/themes/custom/module_5_test/js/slick.js
+++ web/themes/custom/module_5_test/js/slick.js
@@ -5,15 +5,19 @@
       $('.view-id-match_carousel_slider.view-display-id-block_1 .view-content').slick({
         dots: true,
         infinite: false,
-        speed: 300,
-        autoplay: true,
+        // speed: 300000,
+        autoplay: false,
         autoplaySpeed: 2000,
         slidesToShow: 1,
         adaptiveHeight: true,
         prevArrow: '<span class="Slick-Prev arrow"><i class="lni lni-chevron-left"></i></span>',
         nextArrow: '<span class="Slick-Next arrow"><i class="lni lni-chevron-right"></i></span>',
+      }),
+      $('.match__info__title:contains("India"), .match__info__title:contains("IND")').each(function() {
+        $(this).closest('.match').find('.goindia').css('display', 'block');
       });
+
     }
   };
- 
+
 })(jQuery, Drupal);
